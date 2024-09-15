41a555de58a040cc46028cfbd668cdc66129a6c0
sam47933 <kavyanj2024@gmail.com>
2024-09-15 08:45:31 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,98 @@
+info.player1.onScore(15, function () {
+    game.gameOver(false)
+    music.play(music.melodyPlayable(music.powerDown), music.PlaybackMode.UntilDone)
+    game.reset()
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.UntilDone)
+    info.changeScoreBy(1)
+    pause(1000)
+})
+scene.setBackgroundColor(10)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . f . . . f . . . . . . . . 
+    . . . f . . . f . . . . . . . . 
+    . . . f . . . f . . . . . . . . 
+    . . . f . . . f . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . f f f . . . . . . . . . 
+    . . . f . . . f . . . . . . . . 
+    . . . f . . . f . . . . . . . . 
+    . . . f . . . f . . . . . . . . 
+    . . . . f f f . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+let mySprite2 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . 3 3 1 . . . . . . . . 
+    . . . . 3 3 3 3 1 . . . . . . . 
+    . . . . 3 3 . 3 3 . . . . . . . 
+    . . . . 1 3 3 3 3 . . . . . . . 
+    . . . . . 1 3 3 . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Food)
+mySprite.follow(mySprite2)
+controller.moveSprite(mySprite2, 115, 115)
+animation.runImageAnimation(
+mySprite,
+[img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . f . . f . . . . . . . . 
+    . . . . f . . f . . . . . . . . 
+    . . . . f . . f . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . f f . . . . . . . . . 
+    . . . . f . . f . . . . . . . . 
+    . . . . f . . f . . . . . . . . 
+    . . . . . f f . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . f . . f . . . . . . . 
+    . . . . . f . . f . . . . . . . 
+    . . . . . f . . f . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . f f f f . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `],
+500,
+true
+)
+MakeyMakey.setSimulatorKeymap(
+MakeyMakey.PlayerNumber.ONE,
+MakeyMakey.MakeyMakeyKey.UP,
+MakeyMakey.MakeyMakeyKey.DOWN,
+MakeyMakey.MakeyMakeyKey.LEFT,
+MakeyMakey.MakeyMakeyKey.RIGHT,
+MakeyMakey.MakeyMakeyKey.LEFT_CLICK,
+MakeyMakey.MakeyMakeyKey.RIGHT_CLICK
+)
